54cb1e6d209a59be44d6f96a3e2d61db8ce607b8
Manuel <manuel.duenasc@gmail.com>
2025-04-08 13:50:09 +0200

    Primer commit

--- modules/custom/registro_usuario/src/Plugin/Block/UltimosUsuariosRegistradosBloque.php
+++ modules/custom/registro_usuario/src/Plugin/Block/UltimosUsuariosRegistradosBloque.php
@@ -5,6 +5,7 @@ namespace Drupal\registro_usuario\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Database\Database;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+
 /**
  * Define un bloque personalizado.
  *
@@ -14,30 +15,44 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
  * )
  */
 class UltimosUsuariosRegistradosBloque extends BlockBase {
-use StringTranslationTrait;
-    /**
-     * {@inheritdoc}
-     */
-    public function build() {
-        $connection = Database::getConnection();
-        $query = $connection->select('registro_usuario_datos', 'RUD')
-            ->fields('RUD', ['nombre', 'creado'])
-            ->orderBy('RUD.id', 'DESC')
-            ->range(0,5);
-        $result = $query->execute()->fetchAll();
+  use StringTranslationTrait;
 
-        if (!empty($result)) {
-            $message = 'Últimos cinco usuarios registrados:';
-            foreach ($result as $record) {
-                $message .= '<br>Nombre: ' . $record->nombre . '. Registrado: ' . date('Y-m-d h:i:s', $record->creado) . '.';
-            }
-            return [
-                '#markup' => $this->t($message)
-            ];
-        } else {
-            return [
-                '#markup' => $this->t('No se encontraron usuarios registrados.')
-        ];
-        }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // Establecer la conexión a la base de datos.
+    $connection = Database::getConnection();
+    
+    // Hacer la consulta a la base de datos para obtener los últimos 5 usuarios.
+    $query = $connection->select('registro_usuario_datos', 'RUD')
+      ->fields('RUD', ['nombre', 'creado'])
+      ->orderBy('RUD.id', 'DESC')
+      ->range(0, 5);
+    
+    $result = $query->execute()->fetchAll();
+
+    // Si hay resultados, crear el mensaje para mostrar los usuarios.
+    if (!empty($result)) {
+      $message = 'Últimos cinco usuarios registrados:';
+      
+      // Formatear los resultados con array_map.
+      $formatted_users = array_map(function ($record) {
+        return 'Nombre: ' . $record->nombre . '. Registrado: ' . date('Y-m-d h:i:s', $record->creado) . '.';
+      }, $result);
+      
+      // Unir los resultados formateados en un solo mensaje.
+      $message .= implode('<br>', $formatted_users);
+      
+      return [
+        '#markup' => $this->t($message),
+      ];
+    } 
+    // Si no hay resultados, mostrar mensaje alternativo.
+    else {
+      return [
+        '#markup' => $this->t('No se encontraron usuarios registrados.'),
+      ];
     }
+  }
 }
